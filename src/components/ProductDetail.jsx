5c87af995cce4d291515718a74734f8f8db18b98
joy <luckyjoy0306@gmail.com>
2025-04-03 22:07:37 +0800

    qty

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import AddToCart from "./AddToCart"
 
 function ProductDetail({ product }) {
+
+  const [qty, setQty] = useState(product.countInStock > 0 ? 1 : 0);
+
    return (
      <div className="grid grid-cols-1 lg:grid-cols-24 gap-8 justify-center">
        {/* 左側：產品圖片（佔6/24） */}
@@ -22,6 +26,28 @@ function ProductDetail({ product }) {
          <div className="flex flex-col gap-4 text-gray-600">
            <p className="text-2xl font-semibold">US${product.price}.00</p>
            <p className="text-2xl font-semibold">stock：{product.stock}</p>
+
+           <p>
+            <span className="font-bold" >Status</span>: {product.countInStock > 0 ? "In Stock" : "Unavailable."}
+          </p>
+          <div className="flex items-center gap-2">
+            <span className="font-bold" >Qty:</span>
+            <select
+              className="select select-bordered w-20 bg-[#ffffff] text-black"
+              defaultValue={product.countInStock > 0 ? 1 : 0}
+              onChange={ event => setQty(event.target.value)}
+            >
+              {[ ...Array(product.countInStock).keys()].map((x) => (
+                <option key={x + 1} value={x + 1}>
+                  {x + 1}
+                </option>
+              ))}
+            </select>
+          </div>
+          <p>
+            <span className="font-bold">Total Price</span>: {product.price * qty}
+          </p>
+
            <AddToCart />
          </div>
        </div>
